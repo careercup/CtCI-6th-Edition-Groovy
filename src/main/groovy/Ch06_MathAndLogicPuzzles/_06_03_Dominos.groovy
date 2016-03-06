da7655746d256782057353582457ba6c5ea2b7c0
Pap Lőrinc <paplorinc@yahoo.com>
2016-03-06 14:15:35 +0200

    CTCI 6.03: Dominos.groovy

---
+++ src/main/groovy/Ch06_MathAndLogicPuzzles/_06_03_Dominos.groovy
@@ -0,0 +1,7 @@
+package Ch06_MathAndLogicPuzzles
+
+/** Q: If you remove the opposite corners of a chessboard, can you cover it with dominoes that cover two squares? */
+
+/** A: A domino can only cover two squares of different color.
+ *     If we remove the corners, the number of leftover colors won't be equal 
+ */
