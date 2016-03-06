b91727f2fff6a696687ec4ba02f3a9c36740d3b2
Pap Lőrinc <paplorinc@yahoo.com>
2016-03-06 14:16:10 +0200

    CTCI 6.06: BlueEyedIsland.groovy

---
+++ src/main/groovy/Ch06_MathAndLogicPuzzles/_06_06_BlueEyedIsland.groovy
@@ -0,0 +1,13 @@
+package Ch06_MathAndLogicPuzzles
+
+/** Q: All (>0) blue eyed people must leave from an island ASAP.
+ *  Nobody knows their own eye color, just others'.
+ *  How many days will it take them to leave? */
+
+/** A: We will assume that 
+ * 1) all blue eyed people will fit on a single flight
+ * 2) all people are intelligent (or have worked out a plane beforehand).
+ * Since everybody can assume, that if the plane didn't leave after as many days as the number of blue eyed people
+ * the given person sees, she must have a blue eye also.
+ * Therefore they will leave the day after that. */
+
